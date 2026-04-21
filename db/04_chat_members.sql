941b06ef0412b6c3c2e87996abfe25951bd6393e
wrlnduy <wrlnduy@gmail.com>
2026-04-21 03:25:54 +0300

    working now with local pg and redis

--- db/04_chat_members.sql
+++ db/04_chat_members.sql
@@ -9,6 +9,6 @@ $$;
 CREATE TABLE IF NOT EXISTS chat_members (
     chat_id UUID NOT NULL REFERENCES chats(chat_id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-    role chat_role DEFAULT 'member',
+    role chat_role DEFAULT 'MEMBER',
     PRIMARY KEY (chat_id, user_id)
 );
